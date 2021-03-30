1b6819266ebc6f84a38ce242693dcf457658a20e
root <root@web-rpenley.us-east-2.compute.internal>
2021-03-30 01:47:14 +0000

    initial commit of portfolio website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
+import bgVideo from './video/typing_dark_03_videvo.mp4';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div className="App">
+		<video autoPlay muted loop>
+			<source src={bgVideo} type="video/mp4" />
+		</video>
+      <header className="viewpoint-header">
+			<h1> 
+				Ryne Penley
+				<span>Computer Scientist for hire</span>
+			</h1>
+			</header>
+			<div className="footer">
+				Powered by <img src={logo} className="react-logo" alt="logo" />
+			</div>
     </div>
   );
 }
