224b9c18de39cc1e833fc97e3dffeb729d6b27b9
Ryne Penley <rynepenley@gmail.com>
2024-10-24 18:21:26 -0400

    update profile

--- src/App.js
+++ src/App.js
@@ -16,12 +16,12 @@ function App() {
 			<h1>
 				Ryne Penley
 			</h1>
-				<div className="sub">Software and DevOps Engineer</div>
+				<div className="sub">Software and DevSecOps Engineer</div>
 				<div className="linkBar">
 					<a href="https://www.github.com/rpenley" target="_blank" rel="noreferrer"><img src={githubLogo} className="logo" alt="git-logo" /></a>
 					<a href="https://www.linkedin.com/in/rynepenley" target="_blank" rel="noreferrer"><img src={linkedinLogo} className="logo" alt="linkedin-logo" /></a>
 					<a href="https://www.hackerrank.com/rynepenley" target="_blank" rel="noreferrer"><img src={hackerrankLogo} className="logo" alt="hackerrank-logo" /></a>
-					<a href="https://rdp-static.s3.us-east-2.amazonaws.com/Ryne-Penley-Resume.pdf" target="_blank" rel="noreferrer"><img src={resumeLogo} className="logo" alt="resume-logo" /></a>
+					<a href="https://pub-18c80cb185bc47a6bcfd489e5a3e4c7f.r2.dev" target="_blank" rel="noreferrer"><img src={resumeLogo} className="logo" alt="resume-logo" /></a>
 					<a href="mailto:rynepenley@gmail.com"><img src={emailLogo} className="logo" alt="email-logo" /></a>
 				</div>
 			</header>
